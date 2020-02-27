18a6ac5b3a684aeea2e59767a353de889424f432
umbrelluck <yarko.umbrelluck@gmail.com>
2020-02-27 22:24:40 +0200

    faster integral calculator

--- program.cpp
+++ program.cpp
@@ -159,13 +159,13 @@ int checkConfig(config &configs, int inst_check)
         return inst_check;
     if (configs.abs_error < 0 || configs.rel_error < 0)
         return 1;
-    if (configs.abs_error < configs.rel_error)
-    {
-        std::cout << "  ! abs_error < rel_error, switching values...";
-        double tmp = configs.abs_error;
-        configs.abs_error = configs.rel_error;
-        configs.rel_error = tmp;
-    }
+    // if (configs.abs_error < configs.rel_error)
+    // {
+    //     std::cout << "  ! abs_error < rel_error, switching values...";
+    //     double tmp = configs.abs_error;
+    //     configs.abs_error = configs.rel_error;
+    //     configs.rel_error = tmp;
+    // }
     return 0;
 }
 
@@ -180,19 +180,24 @@ bool abs_error(double previous, double res, config &con)
 }
 
 template <typename fun_T>
-double integrate(fun_T fun, config &configs, int step)
+double integrate(fun_T fun, config &configs, int step, int count)
 {
     double d_x = (std::get<1>(configs.x_arr) - std::get<0>(configs.x_arr)) / step;
     double d_y = (std::get<1>(configs.y_arr) - std::get<0>(configs.y_arr)) / step;
     double x, y, res = 0;
-    double tmp;
+    // double tmp;
+    int jump = 1;
 
     for (x = std::get<0>(configs.x_arr); x <= std::get<1>(configs.x_arr); x += d_x)
-        for (y = std::get<0>(configs.y_arr); y <= std::get<1>(configs.y_arr); y += d_y)
+    {
+        jump = (count == 2) ? (jump + 1) % 2 : 0;
+        for (y = std::get<0>(configs.y_arr) + (count - 1 - jump) * d_y; y <= std::get<1>(configs.y_arr); y += d_y * (count - jump))
         {
+            // if (x == -5)
+            // std::cout << "\n";
             res += fun(x, y, configs.size, configs.base_val1, configs.base_val2, configs.coeff) * d_x * d_y;
         }
-
+    }
     return res;
 }
 
@@ -200,6 +205,7 @@ template <typename fun_T>
 double find_best_integral(fun_T fun, config &configs)
 {
     double previous = 0;
+    int count = 1;
     int step = 2;
     bool to_continue = true;
     double rel_err, abs_err;
@@ -208,18 +214,20 @@ double find_best_integral(fun_T fun, config &configs)
     typedef bool (*fn)(double, double, config &);
     fn errors[] = {rel_error, abs_error};
 
-    double res = integrate(fun, configs, step);
+    double res = integrate(fun, configs, step, count);
+    count = 2;
     while (errors[error_index](previous, res, configs))
     // while (to_continue)
     {
-        step *= 2;
         previous = res;
-        res = integrate(fun, configs, step);
+        res = previous / 4;
+        step *= 2;
+        res += integrate(fun, configs, step, count);
 
-        // abs_err = fabs(res - previous);
-        // rel_err = fabs((res - previous) / res);
+        abs_err = fabs(res - previous);
+        rel_err = fabs((res - previous) / res);
 
-        // std::cout << previous << " " << res << " REl error is " << rel_err << "\n";
+        std::cout << previous << " " << res << " REl error is " << rel_err << "\n";
         // // to_continue = (abs_err > configs.abs_error);
         // to_continue = to_continue && (rel_err > configs.rel_error);
     }
@@ -260,7 +268,7 @@ int main(int argc, char **argv)
     }
 
     double value = find_best_integral(function, configs);
-    std::cout << "Result is " << value<<"\n";
+    std::cout << "Result is " << value << "\n";
 
     return 0;
 }
@@ -271,6 +279,7 @@ int main(int argc, char **argv)
 2. errors in reading
 3. time
 4. rel and abs error
+5. size = size_t
 
 
 
