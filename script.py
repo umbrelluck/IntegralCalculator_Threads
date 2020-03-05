f663f26e275ab1d98736f62c522129a086984bf5
umbrelluck <yarko.umbrelluck@gmail.com>
2020-03-05 06:32:05 +0200

    windows version

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
 import subprocess
-from os import system
+from os import system, name
 import re
 
 
-cmd = "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -H./ -B./build -G \"Unix Makefiles\" && cmake --build ./build --config Release --target all"
+cmd = 'cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -H./ -B./build -G "Unix Makefiles" && cmake --build ./build --config Release --target all'
 if __name__ == "__main__":
     system(cmd)
-    cmd="./build/intCalculator " 
+    fileName = ""
+    cmd = 'build\\intCalculator.exe' if name == 'nt' else './build/intCalculator' + \
+        f" {fileName}"
     system(cmd)
